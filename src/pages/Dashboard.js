28b944f33ba875d4cca7c6565709341c007ec250
Wjyeop <wnsduq2178@gmail.com>
2025-05-23 02:05:02 +0900

    feat. 파이어베이스 연동

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,42 +1,11 @@
+import FirstCover from "../components/FirstCover";
+import SecondCover from "../components/SecondCover";
+
 function Dashboard() {
     return(
         <div className="dashboard">
-            <div className="card-wrap">
-                <div className="card">
-                    <div>
-                        <img src="/img/portfolio.png" alt="포트폴리오" />
-                    </div>
-                    <div>
-                        <h1>포트폴리오</h1>
-                    </div>
-                    <div>
-                    <a
-                        className="link"
-                        href="https://www.yigongcoffee.com/"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        링크이동</a>
-                    </div>                    
-                </div>     
-                <div className="card">
-                    <div>
-                        <img src="/img/portfolio.png" alt="포트폴리오" />
-                    </div>
-                    <div>
-                        <h1>포트폴리오</h1>
-                    </div>
-                    <div>
-                    <a
-                        className="link"
-                        href="https://www.yigongcoffee.com/"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        링크이동</a>
-                    </div>                    
-                </div>                    
-            </div>               
+            <FirstCover/>
+            <SecondCover/>
         </div>
     )
 }
